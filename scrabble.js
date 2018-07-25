a8d20a196d1c44105c2e35386a7c6a8bb0a31c13
elliottabirch <elliottabirch@gmail.com>
2018-07-25 16:29:41 -0700

    implement scrabble algorithm

--- scrabble.js
+++ scrabble.js
@@ -1,4 +1,11 @@
 
+// Optimized Solution
+// memoize basic tree of all words provided
+// start recursively creating all permutations of string provided
+//   find current permutation in word tree
+//   if current permutation is not in dictionary, end recursive branch
+//   if current permutation 'isWord', add to results
+
 const addWordToDictionary = (dictionary, word) => {
   if (!word) {
     return { isWord: true, ...dictionary };
@@ -8,62 +15,63 @@ const addWordToDictionary = (dictionary, word) => {
   return dictionary;
 };
 
-const findWordInDictionary = (word, dictionary) => {
-  let currentDictionary = dictionary;
-  if (!word) { return null; }
-  for (const letter of word) {
-    if (currentDictionary[letter]) {
-      currentDictionary = currentDictionary[letter];
-    } else {
-      return null;
-    }
-  }
-  return currentDictionary;
-};
-
-const getValidWords = (string, list) => {
+const memoizeDictionaryTree = (list) => {
   const dictionary = list.reduce(addWordToDictionary, {});
-  const sortedString = string.split('').sort().join('');
-  const foundWords = {};
-  const testWord = (remString, currentString) => {
-    const wordInDictionary = findWordInDictionary(currentString, dictionary);
-    if (wordInDictionary && wordInDictionary.isWord) { foundWords[currentString] = true; }
-    if (wordInDictionary || !currentString) {
-      for (let i = 0; i < remString.length; i++) {
-        const letter = remString[i];
-        if (letter !== string[i - 1]) {
-          testWord(remString.slice(0, i) + remString.slice(i + 1), currentString + letter);
+  return (string) => {
+    const foundWords = {};
+    const testWord = (remString, currentString, currentDictionary) => {
+      if (currentDictionary) {
+        if (currentDictionary.isWord) { foundWords[currentString] = true; }
+        for (let i = 0; i < remString.length; i++) {
+          const letter = remString[i];
+          testWord(remString.slice(0, i) + remString.slice(i + 1), currentString + letter, currentDictionary[letter]);
         }
       }
-    }
+    };
+    testWord(string, '', dictionary);
+    return Object.keys(foundWords);
   };
-  testWord(sortedString, '');
-  return Object.keys(foundWords);
 };
 
+
 const time = new Date();
-getValidWords('acbcdefg', ['abcd', 'abc', 'abcde', 'abcc']);
+const getValidWords = memoizeDictionaryTree(['abcd', 'abc', 'abcde', 'abcc']);
+console.log(getValidWords('acbcdefg'));
+console.log(getValidWords('acbcddfg'));
+console.log(getValidWords('acbcccfg'));
+
 console.log('optimized solution:', time - new Date());
 
 
-const _getValidWords = (string, list) => {
+// Neive solution
+// memoize dictinary of all words
+// iterate through current letters, get all permutations of those letters
+// add permutations found in dictionary to results
+// return results
+
+const _memoizeDictionary = (list) => {
   const dictionary = list.reduce((accum, word) => {
     accum[word] = true;
     return accum;
   }, {});
-  const foundWords = {};
-  const testWord = (remString, currentString) => {
-    if (dictionary[currentString]) { foundWords[currentString] = true; }
-    for (let i = 0; i < remString.length; i++) {
-      const letter = remString[i];
-      testWord(remString.slice(0, i) + remString.slice(i + 1), currentString + letter);
-    }
-  };
+  return (string) => {
+    const foundWords = {};
+    const testWord = (remString, currentString) => {
+      if (dictionary[currentString]) { foundWords[currentString] = true; }
+      for (let i = 0; i < remString.length; i++) {
+        const letter = remString[i];
+        testWord(remString.slice(0, i) + remString.slice(i + 1), currentString + letter);
+      }
+    };
 
-  testWord(string, '');
-  return Object.keys(foundWords);
+    testWord(string, '');
+    return Object.keys(foundWords);
+  };
 };
 
 const _time = new Date();
-_getValidWords('acbcdefg', ['abcd', 'abc', 'abcde', 'abcc']);
+const _getValidWords = _memoizeDictionary(['abcd', 'abc', 'abcde', 'abcc']);
+console.log(_getValidWords('acbcdefg'));
+console.log(_getValidWords('acbcddfg'));
+console.log(_getValidWords('acbcccfg'));
 console.log('neive solution: ', _time - new Date());
